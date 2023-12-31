0a1ced67617425624358dce391f4c29771aa5d85
kumar-abhi001 <123827293+kumar-abhi001@users.noreply.github.com>
2024-01-01 01:20:57 +0530

    New programs added

--- Array/Sorting/InsertionSort.cpp
+++ Array/Sorting/InsertionSort.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int temp = arr[i];
+        int pos = i;
+        for (int j = i - 1; j >= 0; j--) {
+            if(temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                pos = j;
+            }
+        }
+        arr[pos] = temp;
+    }
+
+    cout << "Sorted Array" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     
 }
 
---
+++ Array/String/String.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char arr[] = {'a', 'b', 'c', 'd'};
+    /*
+    arr to string
+    strcpy(arr);
+    c_str();
+    */
+
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+}
